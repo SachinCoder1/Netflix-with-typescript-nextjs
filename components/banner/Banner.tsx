6d73479a46361827542c259d66fb87dea56b9a62
SachinCoder1 <sachinhlo232@gmail.com>
2022-05-02 19:05:16 +0530

    adding banner

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react'
 import { Movie } from '../../typings'
 
 interface Props {
   netflixOriginals: Movie[];
 }
 const Banner = ({ netflixOriginals}:Props) => {
-  return <div>Banner</div>
+    const [movie, setmovie] = useState<Movie | null>(null)
+    useEffect(() => {
+        setmovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
+    }, [netflixOriginals])
+    
+  return (<div>
+      <div>
+          <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+      </div>
+
+  </div>)
 }
 
 export default Banner
