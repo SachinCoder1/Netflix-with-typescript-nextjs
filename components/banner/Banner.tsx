47fa905aadf78fa31536677be369b40b488b08ca
SachinCoder1 <sachinhlo232@gmail.com>
2022-05-02 18:56:23 +0530

    added banner and passing ssr props

---
+++ components/banner/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Movie } from '../../typings'
+
+interface Props {
+  netflixOriginals: Movie[];
+}
+const Banner = ({ netflixOriginals}:Props) => {
+  return <div>Banner</div>
+}
+
+export default Banner
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/banner/Banner'
 import Navbar from '../components/header/Navbar'
-import requests from '../utils/Requests'
+import { Movie } from '../typings'
+import requests from '../utils/requests'
 
-const Home: NextPage = () => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
   return (
     <div
       className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]`}
@@ -15,6 +37,9 @@ const Home: NextPage = () => {
       </Head>
 
       <Navbar />
+      <main>
+        <Banner netflixOriginals={netflixOriginals} />
+      </main>
     </div>
   )
 }
@@ -23,7 +48,7 @@ export default Home
 
 export const getServerSideProps = async () => {
   const [
-    netflixOrigionals,
+    netflixOriginals,
     trendingNow,
     topRated,
     actionMovies,
@@ -32,19 +57,19 @@ export const getServerSideProps = async () => {
     romanceMovies,
     documentaries,
   ] = await Promise.all([
-    fetch(requests.fetchNetflixOriginals).then((res)=>res.json()),
-    fetch(requests.fetchTrending).then((res)=>res.json()),
-    fetch(requests.fetchTopRated).then((res)=>res.json()),
-    fetch(requests.fetchActionMovies).then((res)=>res.json()),
-    fetch(requests.fetchComedyMovies).then((res)=>res.json()),
-    fetch(requests.fetchHorrorMovies).then((res)=>res.json()),
-    fetch(requests.fetchRomanceMovies).then((res)=>res.json()),
-    fetch(requests.fetchDocumentaries).then((res)=>res.json()),
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
   ])
 
   return {
     props: {
-      netflixOrigionals: netflixOrigionals.results,
+      netflixOrigionals: netflixOriginals.results,
       trendingNow: trendingNow.results,
       topRated: topRated.results,
       actionMovies: actionMovies.results,
